7b38a09d89c429c67b969240fd05df6bfb2eeb75
windandgithub <guanyutian2013@gmail.com>
2018-03-24 19:45:23 -0400

    2.1 Class Cube Methods
    Added methods: hasNext, next, rotate, rightRoll, leftRoll, identity.
    Note: I used String[] instead of Color[] FOR NOW becuase the class Color
    has bug, which causes Color[] not working. We can simply replace every
    keyword String with Color later on when we fixed Class Color.
    Note: I did not do the throw exception part for the method hasNext() and
    Next().

--- Cube.java
+++ Cube.java
@@ -1,36 +1,176 @@
 public class Cube {
 	
-	Color[] faces;
+	private String[] faces;
+	private String[] original = new String[7];
+
+	private int counter = 1;
 	
-	public Cube (Color[] faces) {
+	public Cube (String[] faces) {
+
+		// Makes an array that stores the original state
+		for (int i = 1; i <= 6; i++) {
+			original[i] = faces[i];
+		}
+
 		this.faces = faces;
 	}
 	
-	public Color getUp() {
+	public String getUp() {
 		return faces[1];
 	}
 	
-	public Color getFront() {
+	public String getFront() {
 		return faces[2];
 	}
 	
-	public Color getRight() {
+	public String getRight() {
 		return faces[3];
 	}
 	
-	public Color getBack() {
+	public String getBack() {
 		return faces[4];
 	}
 	
-	public Color getLeft() {
+	public String getLeft() {
 		return faces[5];
 	}
 	
-	public Color getDown() {
+	public String getDown() {
 		return faces[6];
 	}
 	
 	public String toString() {
 		return "[ "+getUp()+", "+getFront()+", "+getRight()+", "+getBack()+", "+getLeft()+", "+getDown()+"]";
+	}
+
+	public boolean hasNext() {
+		if (counter > 24) {
+			return false;
+		}
+		return true;
+	}
+
+	public void next() {
+		if (counter == 1) {
+			identity();
+		}
+		else if (counter == 5 || counter == 9 || counter == 21) {
+			rightRoll();
+		}
+		else if (counter == 13 || counter == 17) {
+			leftRoll();
+		}
+		else {
+			rotate();
+		}
+
+		counter ++;
+	}
+
+	public void reset() {
+		for (int i = 1; i <= 6; i++) {
+			faces[i] = original[i];
+		}
+		counter = 1;
+	}
+
+	private void rotate() {
+		int opr = 2;
+		int oprNext = opr + 1;
+		String temp0 = faces[opr], temp1;
+
+		for (int i = 2; i <= 5; i++) {
+
+			if (opr > 5) {
+				opr = 2;
+			}
+			else if (opr == 5) {
+				oprNext = 2;
+			}
+
+			temp1 = faces[oprNext];
+			faces[oprNext] = temp0;
+			temp0 = temp1;
+
+			opr++;
+			oprNext++;
+		}
+	}
+
+	private void rightRoll() {
+		int opr = 1;
+		int oprNext = 3;
+		String temp0 = faces[opr], temp1;
+
+		for (int i = 1; i <= 4; i++) {
+
+			if (i == 2) {
+				opr = 3;
+				oprNext = 6;
+			}
+			else if (i == 3) {
+				opr = 6;
+				oprNext = 5;
+			}
+			else if (i == 4) {
+				opr = 5;
+				oprNext = 1;
+			}
+
+			temp1 = faces[oprNext];
+			faces[oprNext] = temp0;
+			temp0 = temp1;
+		}
+	}
+
+	private void leftRoll() {
+		int opr = 1;
+		int oprNext = 5;
+		String temp0 = faces[opr], temp1;
+
+		for (int i = 1; i <= 4; i++) {
+
+			if (i == 2) {
+				opr = 5;
+				oprNext = 6;
+			}
+			else if (i == 3) {
+				opr = 6;
+				oprNext = 3;
+			}
+			else if (i == 4) {
+				opr = 3;
+				oprNext = 1;
+			}
+
+			temp1 = faces[oprNext];
+			faces[oprNext] = temp0;
+			temp0 = temp1;
+		}
+	}
+
+	private void identity() {
+		reset();
+	}
+
+	// This is used to test the methods
+	public void display() {
+		for (int i = 1; i <= 6; i++) {
+			System.out.println(this.faces[i]);
+		}
+		System.out.println("------------");
+	}
+
+	// This is used to test the methods
+	public static void main(String[] args) {
+		Cube c;
+		c = new Cube(new String[]{"none","BLUE", "GREEN", "WHITE", "GREEN", "BLUE", "RED"});
+
+		while (c.hasNext() == true) {
+			c.next();
+			c.display();
+		}
+		c.reset();
+		c.display();
 	}
 }
