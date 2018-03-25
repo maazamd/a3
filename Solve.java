d1ca485ba1d3a1ee639022dc6f464893698c3a3a
maazamd <37749907+maazamd@users.noreply.github.com>
2018-03-25 16:14:17 -0400

    Add files via upload

--- Solve.java
+++ Solve.java
@@ -1,3 +1,24 @@
 public class Solve {
-
+	
+	public Queue<Solution> generateAndTest() {
+		
+	}
+	
+	public Queue<Solution> breadthFirstSearch() {
+		
+	}
+	
+	public static void main(String[] args) {
+		long start, stop;
+		System.out.println("generateAndTest: ");
+		start = System.currentTimeMillis();
+		generateAndTest();
+		stop = System.currentTimeMillis;
+		System.out.println("Elapsed time: "+(stop-start)+" milliseconds");
+		System.out.println("breadthFirstSearch: ");
+		start = System.currentTimeMillis();
+		breadthFirstSearch();
+		stop = System.currentTimeMillis();
+		System.out.println("Elapsed time: "+(stop-start)+" milliseconds");
+	}
 }
